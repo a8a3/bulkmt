50341023382cadc923eff3f98206991424fea4cc
Alexey Abramov <a.abramov@egopay.ru>
2019-11-14 16:00:11 +0300

    reader implementation

--- sources/bulk.cpp
+++ sources/bulk.cpp
@@ -2,6 +2,50 @@
 #include "bulk_impl.hpp"
 
 // ------------------------------------------------------------------
-int main( int, char** ) {
+int main( int argc, char** argv) {
+
+   std::ios::sync_with_stdio(false);
+
+   if (argc != 2) {
+      std::cout << "incorrect parameters count... \n"
+                   "run as: bulk <N>\n"
+                   "where N is bulk size\n";
+      return 0;
+   }
+
+   const int bulk_size = std::stoi(argv[1]);
+   std::cout << "bulk_size: " << bulk_size << std::endl;
+
+   auto sp = std::make_shared<int>(100);
+
+   auto sp_1(sp);
+   auto sp_2(sp);
+
+   auto pp = std::make_shared<int>(200);
+
+   std::cout << std::boolalpha << sp.owner_before(sp_1) << std::endl  // false
+                               << sp.owner_before(sp_2) << std::endl  // false
+                               << sp.owner_before(pp)   << std::endl  // true
+                               << pp.owner_before(sp)   << std::endl; // false
+
+   auto own_the_same = [] (const auto& p1, const auto& p2) {
+      return !p1.owner_before(p2) && !p2.owner_before(p1);
+   };
+
+   std::cout << std::boolalpha << own_the_same(sp, sp_1)   << std::endl  // true
+                               << own_the_same(sp, sp_2)   << std::endl  // true
+                               << own_the_same(sp_1, sp_2) << std::endl  // true
+                               << own_the_same(pp, sp)     << std::endl; // false
+
+   reader r;
+   auto bulk = std::make_shared<bulk_command>();
+   r.subscribe(bulk);
+   r.read(std::cin);
+   r.unsubscribe(bulk);
+   r.read(std::cin);
+
+
+
+
    return 0;
 }
--- sources/bulk_impl.hpp
+++ sources/bulk_impl.hpp
@@ -1,3 +1,70 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <list>
+#include <memory>
 
+#define TRACE(_MSG_) std::cout << (_MSG_) << std::endl;
+
+// ------------------------------------------------------------------
+class reader_observer {
+public:
+   virtual void notify(const std::string& str) = 0;
+};
+using reader_observer_sptr = std::shared_ptr<reader_observer>;
+using reader_observer_wptr = std::weak_ptr<reader_observer>;
+
+// ------------------------------------------------------------------
+class bulk_command : public reader_observer {
+public:
+   bulk_command() = default;
+  ~bulk_command() = default;
+
+  // i_reader_observer impl
+  void notify(const std::string& str) override {
+     std::cout << str << std::endl;
+  }
+};
+
+// ------------------------------------------------------------------
+class reader {
+public:
+   reader() = default;
+  ~reader() = default;
+
+  void read(std::istream& s) const {
+     std::string str;
+
+     while (s.peek() != '\n' && std::getline(s, str)) {
+        for (auto& observer: observers_) {
+           if (!observer.expired()) {
+              observer.lock()->notify(str);
+           }
+        }
+     }
+     s.ignore();
+  }
+
+  void subscribe(const reader_observer_sptr& observer) {
+     TRACE("add new subscriber");
+     observers_.push_back(observer);
+  }
+
+  void unsubscribe(const reader_observer_sptr& observer) {
+     TRACE("try to unsubscribe subscriber...");
+     const auto sz = observers_.size();
+     observers_.remove_if([&observer](const auto& stored_observer) {
+        return !observer.owner_before(stored_observer) &&
+               !stored_observer.owner_before(observer);
+     });
+     if (sz > observers_.size()) {
+        TRACE("done");
+     } else {
+        TRACE("failed");
+     }
+  }
+
+private:
+   std::list<reader_observer_wptr> observers_;
+};
