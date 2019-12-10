7eae4224d851aa64c575d721575c079ef7131d82
Alexey Abramov <a.abramov@egopay.ru>
2019-12-10 17:03:35 +0300

    add file_printer threads

--- sources/printer.hpp
+++ sources/printer.hpp
@@ -14,8 +14,19 @@
 #include "command.hpp"
 #include "counters.hpp"
 
+struct work_thread {
+   std::thread t_;
+   std::string name_;
+};
+
 // ------------------------------------------------------------------
 class printer {
+protected:
+    std::queue<command_ptr> cmd_queue_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::atomic<bool> stop_{false};
+
 public:
     virtual void print(const command_ptr& cmd) = 0;
     virtual void stop() {}
@@ -26,14 +37,10 @@ using pringer_wptr = std::weak_ptr<printer>;
 
 // ------------------------------------------------------------------
 class console_printer : public printer {
-    std::queue<command_ptr> cmd_queue_;
-    std::mutex mutex_;
-    std::condition_variable cv_;
-    std::atomic<bool> stop_{false};
-    std::thread worker_;
+    work_thread worker_{{}, "console_printer"};
 
-    void do_print() {
-        worker_counters wc("console printer");
+    void do_print(const std::string& thread_name) {
+        worker_counters wc(thread_name);
         
         while (!stop_) {
             // std::cout << "start wait\n";
@@ -78,7 +85,7 @@ class console_printer : public printer {
 public:
     console_printer() {
         // std::cout << "CTOR\n";
-        worker_ = std::thread(&console_printer::do_print, this);
+        worker_.t_ = std::thread(&console_printer::do_print, this, worker_.name_);
     }
 
     ~console_printer() override {
@@ -87,8 +94,8 @@ public:
         stop_ = true;      // stop worker thread and notify it again
         cv_.notify_one();
 
-        if (worker_.joinable()) {
-            worker_.join();
+        if (worker_.t_.joinable()) {
+            worker_.t_.join();
         };
     }
 
@@ -105,19 +112,99 @@ public:
 
 // ------------------------------------------------------------------
 class file_printer : public printer {
-public:
-    file_printer() = default;
- 
-    void print (const command_ptr& cmd) override 
-    {
-        std::cout << "print cmd to file...\n";
-        const auto cmd_creation_time = std::chrono::duration_cast<std::chrono::seconds>(cmd->get_creation_time_point().time_since_epoch());
-        const auto file_name = "bulk" + std::to_string(cmd_creation_time.count()) + ".txt";
-        std::ofstream file(file_name, std::ios::app);
-        file << *cmd;
-        file.close();
+    std::array<work_thread, 2> workers_{work_thread{{}, "work_thread_1"},
+                                        work_thread{{}, "work_thread_2"}};
 
-        // TODO test
-        std::this_thread::sleep_for((std::chrono::seconds{1}));
+    void do_print (const std::string& thread_name)
+    {
+//        std::cout << "print cmd to file...\n";
+//        const auto cmd_creation_time = std::chrono::duration_cast<std::chrono::seconds>(cmd->get_creation_time_point().time_since_epoch());
+//        const auto file_name = "bulk" + std::to_string(cmd_creation_time.count()) + ".txt";
+//        std::ofstream file(file_name, std::ios::app);
+//        file << *cmd;
+//        file.close();
+//
+//       // TODO test
+//       std::this_thread::sleep_for((std::chrono::seconds{1}));
+
+        const auto write_to_file = [&thread_name] (const auto& cmd) {
+           const auto cmd_creation_time = std::chrono::duration_cast<std::chrono::seconds>(cmd->get_creation_time_point().time_since_epoch());
+           const auto file_name = "bulk_" + thread_name + "_" + std::to_string(cmd_creation_time.count()) + ".txt";
+           std::ofstream file(file_name, std::ios::app);
+           file << *cmd;
+           file.close();
+        };
+
+        worker_counters wc(thread_name);
+
+        while (!stop_) {
+           // std::cout << "start wait\n";
+
+           std::unique_lock<std::mutex> u_lock(mutex_);
+           cv_.wait(u_lock, [this](){return !cmd_queue_.empty() || stop_;});
+
+           // std::cout << "wake up\n";
+
+           if (stop_) {
+              // std::cout << "got stop signal\n";
+              break; // while
+           }
+
+           // std::cout << "commands in queue: " << cmd_queue_.size() << std::endl;
+
+           // TODO test
+           std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+           const auto cmd = cmd_queue_.front();
+           cmd_queue_.pop();
+           u_lock.unlock();
+
+
+           write_to_file(cmd);
+           wc.calc(cmd);
+
+        } // while
+
+        std::lock_guard<std::mutex> lock(mutex_);
+        while (!cmd_queue_.empty()) {
+           std::cout << "queue is not empty yet\n";
+
+           const auto cmd = cmd_queue_.front();
+           cmd_queue_.pop();
+           write_to_file(cmd);
+
+           wc.calc(cmd);
+        }
+        std::cout << wc;
     }
+
+public:
+    file_printer() {
+        // std::cout << "CTOR\n";
+        for (auto& worker: workers_) {
+            worker.t_ = std::thread(&file_printer::do_print, this, worker.name_);
+        }
+    }
+
+   ~file_printer() override {
+      // std::cout << "DTOR\n";
+
+      stop_ = true;
+      cv_.notify_all();
+
+      std::for_each(workers_.begin(), workers_.end(), [](auto& worker) {
+         if (worker.t_.joinable()) {
+            worker.t_.join();
+         };
+      });
+   }
+
+   void print(const command_ptr& cmd) override
+   {
+       std::unique_lock<std::mutex> u_lock(mutex_);
+       cmd_queue_.push(cmd);
+       u_lock.unlock();
+       cv_.notify_all();
+
+ //    std::cout << "notify workers\n";
+   }
 };
